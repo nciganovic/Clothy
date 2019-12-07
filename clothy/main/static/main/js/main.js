82043cd596ecdb34c48db193e772759454d10fa6
Nikola Ciganović <nciganovic99@gmail.com>
2019-12-07 16:25:37 +0100

    improved navbar and index.html

--- clothy/main/static/main/js/main.js
+++ clothy/main/static/main/js/main.js
@@ -1,4 +1,3 @@
-//var navbar = document.querySelector('.navbar');
 var navbar = document.getElementsByTagName('nav');
 navbar = navbar[0]
 
@@ -10,20 +9,24 @@ navbar.addEventListener('mouseout', setWhite);
 var items = document.getElementsByClassName('nav-link');
 
 function setBlack(){
-    for(let i = 0; i < items.length; i++){
-        items[i].classList.toggle('text-dark');
-        items[i].classList.remove('text-light');
+    if(!isScrolling){
+        for(let i = 0; i < items.length; i++){
+            items[i].classList.add('text-dark');
+            items[i].classList.remove('text-light');
+        }
+        title.classList.add('text-dark'); 
+        title.classList.remove('text-light');
     }
-    title.classList.toggle('text-dark'); 
-    title.classList.remove('text-light');
 }
 function setWhite(){
-    for(let i = 0; i < items.length; i++){
-        items[i].classList.toggle('text-light');
-        items[i].classList.remove('text-dark');
+    if(!isScrolling){
+        for(let i = 0; i < items.length; i++){
+            items[i].classList.add('text-light');
+            items[i].classList.remove('text-dark');
+        }
+        title.classList.add('text-light');
+        title.classList.remove('text-dark');
     }
-    title.classList.toggle('text-light');
-    title.classList.remove('text-dark');
 }
 
 var navbarBtn = document.querySelectorAll('.navbar-toggler');
@@ -45,4 +48,22 @@ function setNavbar(){
         isFullScreen = false;
     }
 }
+
+var isScrolling = false;
+
+$( document ).ready(function() {
+    $(window).scroll(function () {
+        if ($(this).scrollTop()  > 5 ){
+            $(".navbar").addClass('set-white');
+            setBlack();
+            isScrolling = true;
+        }
+        else{
+            $(".navbar").removeClass('set-white');
+            isScrolling = false;
+            setWhite();
+            console.log('Navbar on top.');
+        }
+    });
+});
 
