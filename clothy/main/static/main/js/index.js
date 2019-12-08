cbff1741b09ea8272e60179f5582700d445be781
Nikola Ciganović <nciganovic99@gmail.com>
2019-12-08 17:18:58 +0100

    featured product on index.html added

--- clothy/main/static/main/js/index.js
+++ clothy/main/static/main/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    console.log('index.js');
+    /* Zoom in and zoom out on hover */
     $(".img-holder").on('mouseover', function(){
         $(this).find("img").toggleClass('zoom-in');
         $(this).find("img").toggleClass('zoom-out');
@@ -9,4 +9,56 @@ $(document).ready(function() {
         $(this).find("img").toggleClass('zoom-in');
         $(this).find("img").toggleClass('zoom-out');
     })
-});
+
+    /* Change featured image */
+    var currentImg = 0;
+
+    function changePic(currentImg){
+        if(currentImg == 2){
+            console.log('red');
+        }
+        else if(currentImg == 0){
+            console.log('orange');
+        }
+        else if(currentImg == 1){
+            console.log('gray');
+        }
+        var numberOfPhotos = $(".featured-item").length;
+        $(".featured-item").eq(currentImg).fadeToggle(function(){
+            $(".featured-item").each(function(i){
+                $(this).css(
+                    'zIndex', ((numberOfPhotos - i) + currentImg) % numberOfPhotos
+                )
+            })
+            $(this).show();
+        })
+    }
+
+    changePic(currentImg);
+
+    $("#left-arrow").on('click', function(event){
+        event.preventDefault();
+        currentImg--;
+        if(currentImg > 2){
+            currentImg = 0;
+        }
+        if(currentImg < 0){
+            currentImg = 2;
+        }
+        changePic(currentImg);
+    })
+
+    $("#right-arrow").on('click', function(event){
+        event.preventDefault();
+        currentImg++;
+        if(currentImg > 2){
+            currentImg = 0;
+        }
+        if(currentImg < 0){
+            currentImg = 2;
+        }
+        
+        changePic(currentImg);
+    })
+    
+})
