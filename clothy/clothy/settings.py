c9778f5fe9637d054e421de81a2b2cabaa589965
Nikola Ciganović <nciganovic99@gmail.com>
2019-12-11 16:05:45 +0100

    djanog set to mysql

--- clothy/clothy/settings.py
+++ clothy/clothy/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'clothy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
+        'NAME': 'clothy_db',
+        'USER': 'nciganovic',
+        'PASSWORD': '3108999nc',
+
     }
 }
 
@@ -106,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Belgrade'
 
 USE_I18N = True
 
