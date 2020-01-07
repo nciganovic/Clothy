3ba53570d0a86b06eb39a72763490e2515b1cdcc
Nikola Ciganović <nciganovic99@gmail.com>
2020-01-07 23:25:55 +0100

    Amazon web services added

--- clothy/clothy/settings.py
+++ clothy/clothy/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
     'django.contrib.sites',
+    'storages',
 ]
 
 SITE_ID = 1
@@ -150,3 +151,17 @@ STATICFILES_DIRS = [
 
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_PUBLISHABLE_KEY = os.environ.get('STRIPE_PUBLISHABLE_KEY')
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('CLOTHYY_AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATICFILES_STORAGE = 'clothy.storage.S3Storage'
+
+AWS_S3_REGION_NAME = 'eu-west-2'
+AWS_S3_HOST = 's3.eu-west-2.amazonaws.com'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
