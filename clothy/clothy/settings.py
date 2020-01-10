ccfcba2c6cf718eba61fd154456d39b6aca2fdd2
Nikola Ciganović <nciganovic99@gmail.com>
2020-01-10 08:49:28 +0100

    changing settings for deployment on linode

--- clothy/clothy/settings.py
+++ clothy/clothy/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a9mq@l_xfi7+a78r#b7h+tpzuj3_p5wa9$7uwjt(y^mwfuwu(('
+SECRET_KEY =  os.environ.get("CLOTHYY_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
